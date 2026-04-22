9a2c0a422b2082acc85ad25af0326cf691d1869d
Incogninto1 1 <incogninto123@gmail.com>
2026-04-22 09:51:23 +0300

    Added comments to the editor

--- drawing/gui.py
+++ drawing/gui.py
@@ -1,46 +1,53 @@
 #!/usr/bin/env python3
 
-#class pixel:
-#    def __init__(self, x, y):
-#        self.x=
+class pixel:
+    def __init__(self, x, y):
+        self.x= x
+        self.y= y
+        self.Next= None
+
 
 import pygame
 
 print("H3110, C4P7A1N!")
 
-print("W3LC0M3 70 TH3 H7ML P07YG0N BU1LD3R")
+print("W3LC0M3 70 TH3 4RDU1N0 P07YG0N BU1LD3R")
 
-pygame.init()
+pygame.init()                                           #Start pygame
 
-window = pygame.display.set_mode((768, 960))
-X1 = pygame.time.Clock()
+window = pygame.display.set_mode((768, 960))            #Make the pygame window
+X1 = pygame.time.Clock()                                #Main pygame timer
 
-pygame.display.set_caption("HTML Polygon Builder")
+pygame.display.set_caption("Arduino Polygon Builder")   #Set the title
 
-run=True
+run=True                                                #flag to tell that the program is running
 
 
-x=0
+x=0                                                     #Cursor location variables
 y=0
-while run:
+while run:                                              #while the program is running
     pygame.time.delay(60)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run=False
+    for event in pygame.event.get():                    #read events that pygame recognises
+        if event.type == pygame.QUIT:                   #If we press the X button
+            run=False                                   #The program is no longer running
 
-        elif event.type == pygame.MOUSEBUTTONUP:
-            x, y=event.pos
-            print("{"+str(x//6)+","+str(y//6)+"}, ",end='')
-            pygame.draw.rect(window, (255,255,255), (x, y, 6, 6))
+        elif event.type == pygame.MOUSEBUTTONUP:                    #If the user clicks the mouse
+            x, y=event.pos                                          #Get the coordinates of the cursor
+            print("{"+str(x//6)+","+str(y//6)+"}, ",end='')         #TODO: Implement a stack and add each node to the stack
+            pygame.draw.rect(window, (255,255,255), (x, y, 6, 6))   #Place a size accurate pixel
+                                                                    #TODO: place the pixels in the same way as the LCD for a WYSIWYG experience
 
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_s:
-                print("}")
+        elif event.type == pygame.KEYUP:                            #If the user presses a key
+            if event.key == pygame.K_s:                             #If that key is the 's' key on the keyboard (Save)
+                print("}")                                          #Placeholder (for now we print it on the terminal window)
+                                                                    #TODO(maybe): Save the final structure to a TXT file
+            elif event.key == pygame.K_c:                           #If the key presses is 'c' (Compile)
+                print("WIP")                                        #TODO: Make a basic compiler that draws a preview 
 
-    pygame.display.update()
-    X1.tick(60)
+    pygame.display.update()                                         #Refresh the screen
+    X1.tick(60)                                                     #Wait for the timer
 
-pygame.quit()
+pygame.quit()                                                       #Quit the editor
 
 print("533 U L8R, 4L1G470R")
